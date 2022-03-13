3ceb71606ee8c225c9ee5d77569777b01f939049
Spoon2 <viktor.spoon2@gmail.com>
2022-03-13 08:26:03 +0400

    task 1 created

---
+++ seminar3/task1/task1.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Программа для выявление палиндромов из 5-ти значных чисел");
+Console.WriteLine("Введите 5-ти значное число");
+int b = int.Parse(Console.ReadLine());
+if(b > 99999 && b < -99999 && b<10000 && b> -10000){
+    Console.WriteLine("Вы ввели некорректное число");
+}
+string strb = b.ToString();
+
+if(strb[0]==strb[strb.Length-1]&strb[1]==strb[strb.Length-2]){
+    Console.WriteLine("Число - палиндром");
+}   else{
+    Console.WriteLine("Число - не палиндром");
+    } 
