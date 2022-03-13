e8a745c4225fb9ce509ad3d50ab5846e4ae05dd0
Spoon2 <viktor.spoon2@gmail.com>
2022-03-13 08:27:55 +0400

    task1.1 created

---
+++ seminar3/task1.1/task1.1.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Программа для определения палиндромов. Введите число");
+
+string num = Convert.ToString(Console.ReadLine());
+char[]chr = num.ToCharArray();
+char[]chr1 = num.ToCharArray();
+
+Array.Reverse(chr1);
+
+Console.WriteLine(chr1);
+int ans = 0;
+for(int i =0; i<chr.Length; i++){
+for(int j = chr.Length-1;j>0; j--){    
+if(chr1[i]==chr[j]){
+    ans = 1;
+}   else{
+    ans = 0;
+}
+}
+}
+   if(ans == 1){
+    Console.WriteLine("Число - палиндром");
+}   else{
+    Console.WriteLine("Число - не палиндром");
+}
