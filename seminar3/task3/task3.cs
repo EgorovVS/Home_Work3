4eaa53a687dcea576324c64f15a866b7bdea8abf
Spoon2 <viktor.spoon2@gmail.com>
2022-03-13 08:32:10 +0400

    task3 created

---
+++ seminar3/task3/task3.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Программа принимает число N, затем выводит кубы чисел от 1 до N");
+int N, i, j;
+
+i = 1;
+j = 0;
+Console.WriteLine("Введите N");
+
+N = Convert.ToInt32(Console.ReadLine());
+
+for(i=1; i<N+1; i++){
+    j = i;
+       Console.WriteLine("Куб числа " + j + " равен "+ j*j*j);
+}     
+        
